cf8007d0f92ccf6d10ad5c952c28b4009ac4ae52
Hafidz <halfpheez92@gmail.com>
2018-09-25 23:01:36 +0800

    updated code, added getter method.

--- advanced-ruby-revision/rubyClasses.rb
+++ advanced-ruby-revision/rubyClasses.rb
@@ -4,10 +4,16 @@ system "clear"
 class Square
     # initialize method.
     def initialize(side_length)
-        @side_length = side_length # defines an instance variable.
+      @side_length = side_length # defines an instance variable.
+    end
+
+    # defines getter.
+    def side_length
+        return @side_length
     end
 end
 
-# instantiates Square class.
+# instantiates Square class with args.
 mySquareObject = Square.new(10)
 puts mySquareObject.inspect
+puts mySquareObject. # gets side length.
