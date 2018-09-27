84db4cbcc9ddd22971cecf276c9375711318cb24
Hafidz <halfpheez92@gmail.com>
2018-09-27 18:24:15 +0800

    added more class methods.

--- advanced-ruby-revision/rubyClasses.rb
+++ advanced-ruby-revision/rubyClasses.rb
@@ -2,20 +2,37 @@ system "clear"
 
 # creates Class in Ruby.
 class Square
+
     # initialize method.
     def initialize(side_length)
         @side_length = side_length # defines an instance variable.
     end
 
-    # defines getter
+    # defines getter.
     def side_length
         return @side_length
     end
 
-    # defines setter
+    # defines setter.
     def side_length=(side_length)
         @side_length = side_length
     end
+
+    # defines a new function, perimeter.
+    def perimeter
+        return @side_length * 4
+    end
+
+    # defines a new function, area.
+    def area
+        return @side_length * @side_length
+    end
+
+    # defines interger to string.
+    def to_s
+       return "\nSide length: #{@side_length}\nPerimeter: #{perimeter}\nArea: #{area}"
+    end
+
 end
 
 # instantiates Square class.
@@ -24,5 +41,9 @@ puts mySquareObject.inspect
 puts mySquareObject.side_length
 
 # set a new value.
-mySquareObject.side_length = 100
+mySquareObject.side_length = 40
 puts "New value of side length is #{mySquareObject.side_length}"
+puts "The perimeter is #{mySquareObject.perimeter}"
+puts "The area is #{mySquareObject.area}"
+
+puts mySquareObject
