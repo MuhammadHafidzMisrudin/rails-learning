b2fff0a35d2ceda449df77d80842e30da7baf64b
Hafidz <halfpheez92@gmail.com>
2018-09-27 17:55:53 +0800

    added setter method in ruby.

--- advanced-ruby-revision/rubyClasses.rb
+++ advanced-ruby-revision/rubyClasses.rb
@@ -4,16 +4,25 @@ system "clear"
 class Square
     # initialize method.
     def initialize(side_length)
-      @side_length = side_length # defines an instance variable.
+        @side_length = side_length # defines an instance variable.
     end
 
-    # defines getter.
+    # defines getter
     def side_length
         return @side_length
     end
+
+    # defines setter
+    def side_length=(side_length)
+        @side_length = side_length
+    end
 end
 
-# instantiates Square class with args.
-mySquareObject = Square.new(10)
+# instantiates Square class.
+mySquareObject = Square.new(20)
 puts mySquareObject.inspect
-puts mySquareObject. # gets side length.
+puts mySquareObject.side_length
+
+# set a new value.
+mySquareObject.side_length = 100
+puts "New value of side length is #{mySquareObject.side_length}"
