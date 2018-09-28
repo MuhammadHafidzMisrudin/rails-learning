7ef1762c15df47f05c19baa592ac103f17e2aa02
Hafidz <halfpheez92@gmail.com>
2018-09-28 15:30:37 +0800

    modified code setter and getter methods using att_accessor.

--- advanced-ruby-revision/rubyClasses.rb
+++ advanced-ruby-revision/rubyClasses.rb
@@ -3,32 +3,37 @@ system "clear"
 # creates Class in Ruby.
 class Square
 
+    # defines setter and getter methods for instance variables.
+    # so doesn't need to redefine these metods individually.
+    attr_accessor :side_length, :new_var
+
     # initialize method.
-    def initialize(side_length)
+    def initialize(side_length, new_var)
         @side_length = side_length # defines an instance variable.
+        @new_var = new_var # adds a new instance variable.
     end
 
-    # defines getter.
-    def side_length
-        return @side_length
-    end
+    # defines getter
+    # def side_length
+    #     return @side_length
+    # end
 
-    # defines setter.
-    def side_length=(side_length)
-        @side_length = side_length
-    end
+    # defines setter
+    # def side_length=(side_length)
+    #     @side_length = side_length
+    # end
 
-    # defines a new function, perimeter.
+    # defines a new function, perimeter
     def perimeter
         return @side_length * 4
     end
 
-    # defines a new function, area.
+    # defines a new function, area
     def area
         return @side_length * @side_length
     end
 
-    # defines interger to string.
+    # defines interger to string
     def to_s
        return "\nSide length: #{@side_length}\nPerimeter: #{perimeter}\nArea: #{area}"
     end
@@ -36,12 +41,13 @@ class Square
 end
 
 # instantiates Square class.
-mySquareObject = Square.new(20)
+mySquareObject = Square.new(20, "NEW 2ND VAR ADDED")
 puts mySquareObject.inspect
 puts mySquareObject.side_length
+puts mySquareObject.new_var
 
 # set a new value.
-mySquareObject.side_length = 40
+mySquareObject.side_length = 50
 puts "New value of side length is #{mySquareObject.side_length}"
 puts "The perimeter is #{mySquareObject.perimeter}"
 puts "The area is #{mySquareObject.area}"
