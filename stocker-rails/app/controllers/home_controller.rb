3b85034678a30b2f947c1a6751c68306b763c19c
MuhammadHafidzMisrudin <halfpheez92@gmail.com>
2018-10-09 22:42:14 +0800

    updated code, connecting form to the HomeController of index method.

--- stocker-rails/app/controllers/home_controller.rb
+++ stocker-rails/app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @stock = StockQuote::Stock.quote("symbol")
+    # params -> it is the information that is sent by forms.
+    # if params/information is equal to nothing, create an instance variable of warning.
+    if params[:id] == ""
+      @nothing = "Error, you forgot to enter a symbol"
+    elsif
+      # if params does have something, store that information into a new instance variable.
+      if params[:id]
+        @stock = StockQuote::Stock.quote(params[:id])
+      end
+    end
   end
 
   def about
