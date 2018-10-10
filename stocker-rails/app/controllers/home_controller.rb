0c5d2ba72702feac445a782dfbf7c68b4a7c0000
MuhammadHafidzMisrudin <halfpheez92@gmail.com>
2018-10-10 23:40:29 +0800

    updated HomeController, added stock quote error handling for valid information.

--- stocker-rails/app/controllers/home_controller.rb
+++ stocker-rails/app/controllers/home_controller.rb
@@ -3,11 +3,18 @@ class HomeController < ApplicationController
     # params -> it is the information that is sent by forms.
     # if params/information is equal to nothing, create an instance variable of warning.
     if params[:id] == ""
-      @nothing = "Error, you forgot to enter a symbol"
+      @nothing = "Error, you forgot to enter a symbol."
     elsif
       # if params does have something, store that information into a new instance variable.
       if params[:id]
-        @stock = StockQuote::Stock.quote(params[:id])
+        # adds a block of error handling to check valid information.
+        begin
+          @stock = StockQuote::Stock.quote(params[:id])
+        rescue Exception => e
+          puts e.message
+          puts e.backtrace.inspect
+          @error = "That Stock Symbol does not exit...please try again."
+        end
       end
     end
   end
